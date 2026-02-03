f795cb6af150a87c31d87a7c1cacfd209edc3fb3
Andrés Contreras Guillén <me@ancongui.com>
2026-02-03 11:48:56 +0100

    feat(demo-app): update routing for component showcase
    - Add routes for all showcase pages (buttons, forms, feedback, data-display, navigation, layouts)
    - Configure lazy loading for all page components
    - Set overview as the default route
    - Remove old home component

--- apps/demo-app/src/app/app.routes.ts
+++ apps/demo-app/src/app/app.routes.ts
@@ -10,7 +10,44 @@ export const appRoutes: Route[] = [
   {
     path: '',
     loadComponent: () =>
-      import('./pages/home/home.component').then((m) => m.HomeComponent),
+      import('./layout/shell.component').then((m) => m.ShellComponent),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/overview/overview.component').then((m) => m.OverviewComponent),
+      },
+      {
+        path: 'buttons',
+        loadComponent: () =>
+          import('./pages/buttons/buttons.component').then((m) => m.ButtonsComponent),
+      },
+      {
+        path: 'forms',
+        loadComponent: () =>
+          import('./pages/forms/forms.component').then((m) => m.FormsComponent),
+      },
+      {
+        path: 'feedback',
+        loadComponent: () =>
+          import('./pages/feedback/feedback.component').then((m) => m.FeedbackComponent),
+      },
+      {
+        path: 'data-display',
+        loadComponent: () =>
+          import('./pages/data-display/data-display.component').then((m) => m.DataDisplayComponent),
+      },
+      {
+        path: 'navigation',
+        loadComponent: () =>
+          import('./pages/navigation/navigation.component').then((m) => m.NavigationComponent),
+      },
+      {
+        path: 'layouts',
+        loadComponent: () =>
+          import('./pages/layouts/layouts.component').then((m) => m.LayoutsComponent),
+      },
+    ],
   },
   {
     path: '**',
